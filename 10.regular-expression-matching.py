03bcdb22c7492c306b32bdc1aed9694dd7ca0afb
Anderson Tsai <tiger0922@gmail.com>
2020-03-16 16:57:13 +0800

    Finished recursive version of No.10

--- 10.regular-expression-matching.py
+++ 10.regular-expression-matching.py
@@ -7,32 +7,28 @@
 # @lc code=start
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        s_pos, p_pos = 0, 0
-        ch_star = ''
-        fail = False
-        while fail == False and s_pos < len(s) and p_pos < len(p):
-            if s[s_pos] == p[p_pos] or p[p_pos] == '.':
-                s_pos = s_pos + 1 
-                p_pos = p_pos + 1
-            elif p[p_pos] == '*':
-                ch_star = p[p_pos-1]
-                if s[s_pos] != ch_star and ch_star != '.':
-                    p_pos = p_pos + 1
-                else:
-                    s_pos = s_pos + 1
-            elif p_pos + 1 < len(p) and p[p_pos+1] == "*":
-                p_pos = p_pos + 1
+        if not s and not p:
+            return True
+        elif len(s) == 1 and len(p) == 1:
+            if s[0] == p[0] or p[0] == '.':
+                return True
             else:
-                fail = True
-        if (p_pos != len(p) or s_pos != len(s)) and p[-1] != "*" and ch_star != '.' and ch_star != s[-1]:
-            fail = True
-        elif ch_star == '.' and p[-1] != s[-1] and p[-1] != "*":
-            fail = True
-        if s_pos < len(s):
-            if s[s_pos] != ch_star and p[-1] == "*":
-                fail = True
+                return False
+        elif len(p) > 1 and p[1] != '*':
+            if not s:
+                return False
+            elif s[0] == p[0] or p[0] == '.':
+                return self.isMatch(s[1:], p[1:]) 
+            else:
+                return False
+        elif len(p) > 1 and p[1] == '*':
+            if(self.isMatch(s, p[2:])): 
+                return True
+            if len(s) > 0 and (s[0] == p[0] or p[0] == '.'):
+                return self.isMatch(s[1:], p)
+        else:
+            return False
 
-        return not fail
     
 # @lc code=end
 
